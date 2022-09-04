0fddc03730c065d9d7010ac9366dcda077ca757f
Ese Kenneth Mudiaga <kenesecorp@hotmail.com>
2022-09-04 01:20:14 +0100

    Switch to controllers

---
+++ AbacusUser.Web/ExceptionFilter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text.Json;
+
+namespace AbacusUser.Web;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    private readonly ILogger<ExceptionFilter> logger;
+
+    public ExceptionFilter(ILogger<ExceptionFilter> logger)
+    {
+        this.logger = logger;
+    }
+    public void OnException(ExceptionContext context)
+    {
+        switch (context.Exception)
+        {
+            case ApplicationException appEx:
+                context.Result = new BadRequestObjectResult(appEx.Message);
+                logger.LogError("Application Exception for request {path} - {message}:: StackTrace:: {stacktrace}",
+                    context.HttpContext.Request.Path, appEx.Message, appEx.StackTrace);
+                break;
+            default:
+                context.Result = new ObjectResult("Oops! Something went wrong.") { StatusCode = 500 };
+                var message = context.Exception.GetBaseException()?.Message ?? "Oops! Something went wrong.";
+                logger.LogError("Application Exception for request {path} - {message}:: StackTrace:: {stacktrace}",
+                    context.HttpContext.Request.Path, message, context.Exception.StackTrace);
+                break;
+        }
+        context.ExceptionHandled = true;
+    }
+}
--- AbacusUser.Web/Program.cs
+++ AbacusUser.Web/Program.cs
@@ -1,14 +1,13 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Annotations;
 using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "AbacusUser.Web", Version = "v1" });
@@ -41,58 +40,19 @@ builder.Services.AddSwaggerGen(c =>
     c.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();
 });
 
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy => policy.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin());
-});
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddLogging(config =>
-{
-    //TO DO: add app-insights
-    config.AddConsole();
-});
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-//TODO : Use swagger conditionally
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast",
-    [SwaggerOperation("Get weather forecast")]
-    [SwaggerResponse(StatusCodes.Status200OK, "Request successful", typeof(WeatherForecast[]))]
-    () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-           new WeatherForecast
-           (
-               DateTime.Now.AddDays(index),
-               Random.Shared.Next(-20, 55),
-               summaries[Random.Shared.Next(summaries.Length)]
-           ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
